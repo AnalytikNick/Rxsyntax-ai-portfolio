406f38d248825d2bd2b4a6bcb201485516933cf4
AnalytikNick <nichodemusihuoma@gmail.com>
2026-03-20 09:04:45 +0100

    Day1: Variables and Data Types - patient Record

---
+++ RxSyntax Ai portfolio.py
@@ -0,0 +1,35 @@
+print("My name is Nick")
+print("I am building RxSyntax AI portfolio")
+print("Day 1 starts now")
+
+print("I am a Pharmacist")
+print("I am a Healthcare Data Analyst")
+print("My goal: Healthcare AI Engineer")
+print("Nothing stops me now")
+
+
+patient_name = "Nick"
+patient_age = 30
+patient_condition = "Healthy"
+is_pharmacist = True
+
+
+print("Patient Name:", patient_name)
+print("Patient Age:", patient_age)
+print("Patient Condition:", patient_condition)
+print("Is Pharmacist:", is_pharmacist)  
+
+
+Hba1c = 5.5
+drug_dose_mg = 500
+patient_weight_kg = 70
+
+print("Hba1c level:", Hba1c)
+print("Drug Dose (mg):", drug_dose_mg)
+print("Patient Weight (kg):", patient_weight_kg)
+
+print(type(patient_name))
+print(type(patient_age))
+print(type(patient_condition))
+print(type(is_pharmacist))
+print(type(Hba1c))
